8087097cf45ee9c42f8c303892fa24ad35b25de7
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-11-24 19:54:43 -0300

    Corrige bug da vírgula na resposta da API

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -13,9 +13,9 @@ export class LoginService {
     return this.http.post('https://private-3923c4-santandercoders809.apiary-mock.com/login', user, {responseType: 'text'}).subscribe((response) => {
       console.log(response);
       // the response is broken, insert missing comma
-      let comma_location = response.search("user") - 2
-      response = response.slice(0, comma_location) + "," + response.slice(comma_location)
-      return JSON.parse(response);
+      // let comma_location = response.search("user") - 2
+      // response = response.slice(0, comma_location) + "," + response.slice(comma_location)
+      return response
     });
   }
 }
