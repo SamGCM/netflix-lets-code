c80120920fbd4d3e505fa7c039e41e79e0db95c0
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-11-23 23:02:57 -0300

    Adiciona classe de validação

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.signupForm = this.fb.group({
         email: ['', [Validators.required, Validators.email]],
-        password: ['', Validators.required]
+        password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(60)]]
     })
   }
 
