1f57868c03bc5ebd79578f2120b63ede0adf31c9
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-11-25 11:55:19 -0300

    Adiciona validação de telefone

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user/user.model';
 import { LoginService } from 'src/app/services/login.service';
@@ -16,9 +16,19 @@ export class LoginComponent implements OnInit {
 
   constructor(private loginService: LoginService, private fb:FormBuilder, private router:Router) { }
 
+  customValidator(control: AbstractControl){
+    let email = Validators.email(control)
+    let number = Validators.pattern("[0-9]{11}")(control)
+    if(email == null || number == null) {
+      return null
+    } else {
+      return {...email, ...number}
+    }
+  }
+
   ngOnInit(): void {
     this.signupForm = this.fb.group({
-        email: ['', [Validators.required, Validators.email]],
+        email: ['', [Validators.required, this.customValidator]],
         password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(60)]]
     })
   }
