56cf64298477f711977e3821300854e60c5c3dc5
ig0rfranca <igorruizdefranca@gmail.com>
2021-11-24 22:15:55 -0300

    - Exclusão de arquivos.

--- src/app/navegation-screen/navegation-screen.module.ts
+++ src/app/navegation-screen/navegation-screen.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BannerAreaComponent } from './banner-area/banner-area.component';
-import { ButtonComponent } from 'src/app/components/button/button.component';
+// import { ButtonComponent } from 'src/app/components/button/button.component';
 import { PageNavegationScreenComponent } from './page-navegation-screen/page-navegation-screen.component';
 import { CarouselAreaComponent } from './carousel-area/carousel-area.component';
 
@@ -14,7 +14,7 @@ import { CarouselAreaComponent } from './carousel-area/carousel-area.component';
     BannerAreaComponent,
     CarouselAreaComponent,
     PageNavegationScreenComponent,
-    ButtonComponent
+    // ButtonComponent
   ]
 })
 export class NavegationScreenModule { }
